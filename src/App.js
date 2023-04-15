c777cb65ab5ae89dbaefc678a11665739d1de987
senbagaraman04 <senbagaraman04@gmail.com>
2023-04-15 19:44:04 +0530

    learn: custom events

--- src/App.js
+++ src/App.js
@@ -15,8 +15,20 @@ function Btn(){
 }
 
 function App() {
+  function handleClick() {
+        let randomNum = Math.floor(Math.random() * 3) + 1;
+        console.log(randomNum);
+        let userInput = prompt('type a number');
+        alert(`Computer number: ${randomNum}, Your guess: ${userInput}`);
+      }
+    
    return (
-   <><Header /><Btn /></>
+   <><Header /><Btn />
+     <div>
+      <h1>Task: Add a button and handle a click event</h1>
+      <button onClick={handleClick}>Guess the number between 1 and 3</button>
+    </div>
+   </>
    );
 }
 
