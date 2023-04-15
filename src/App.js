4f99722f7bbf330af62e2f481a4dabc92b7d4b43
senbagaraman04 <senbagaraman04@gmail.com>
2023-04-15 19:02:23 +0530

    learn: Mouse events

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function Header(){
   return <h1>hello world</h1>;
 }
 
+function Btn(){
+  const clickBtn = () => alert("clicked");
+  const OverBtn = () => console.log("OverBtn");
+  return (
+   
+
+        <button onClick={clickBtn}  onMouseOver={OverBtn}> Click Me</button>
+  );
+}
+
 function App() {
-   return <Header/>
+   return (
+   <><Header /><Btn /></>
+   );
 }
 
 export default App;
